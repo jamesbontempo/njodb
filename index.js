aa75bc7f8e5c0bcba0b3031cc07ee4996d298994
James BonTempo <jbontempo@onpointlearning.com>
2021-02-18 20:24:45 -0500

    Added experimental, undocumented aggregate method; bumped version

--- index.js
+++ index.js
@@ -206,6 +206,70 @@ class Database {
         });
     }
 
+    aggregate(selecter, indexer, field) {
+        if (!(selecter && typeof selecter === "function")) throw new Error("selecter must be defined");
+        if (!(indexer && typeof indexer === "function")) throw new Error("indexer must be defined");
+
+        return new Promise ((resolve, reject) => {
+            var promises = [];
+
+            for (var i = 0; i < this.properties.datastores; i++) {
+                const storepath = path.join(this.properties.datapath, [this.properties.dataname, i, "json"].join("."));
+                promises.push(njodb.aggregateStoreData(storepath, selecter, indexer, field, this.properties.lockoptions, this.properties.debug));
+            }
+
+            Promise.all(promises)
+                .then(results => {
+                    var aggregate = {};
+                    var start = Date.now();
+                    var end = 0;
+                    for (var i = 0; i < results.length; i++) {
+                        const indexes = Object.keys(results[i].aggregate);
+                        for (var j = 0; j < indexes.length; j++) {
+                            if (aggregate[indexes[j]]) {
+                                if (results[i].aggregate[indexes[j]]["min"] < aggregate[indexes[j]]["min"]) aggregate[indexes[j]]["min"] = results[i].aggregate[indexes[j]]["min"];
+                                if (results[i].aggregate[indexes[j]]["max"] > aggregate[indexes[j]]["max"]) aggregate[indexes[j]]["max"] = results[i].aggregate[indexes[j]]["max"];
+                                if (results[i].aggregate[indexes[j]]["m2"] !== undefined) {
+                                    const n = aggregate[indexes[j]]["count"] + results[i].aggregate[indexes[j]]["count"];
+                                    const delta = results[i].aggregate[indexes[j]]["mean"] - aggregate[indexes[j]]["mean"];
+                                    const m2 = aggregate[indexes[j]]["m2"] + results[i].aggregate[indexes[j]]["m2"] + (Math.pow(delta, 2) * ((aggregate[indexes[j]]["count"] * results[i].aggregate[indexes[j]]["count"]) / n));
+                                    aggregate[indexes[j]]["m2"] = m2;
+                                    aggregate[indexes[j]]["varp"] = m2 / n;
+                                    aggregate[indexes[j]]["vars"] = m2 / (n - 1);
+                                    aggregate[indexes[j]]["stdp"] = Math.sqrt(m2 / n);
+                                    aggregate[indexes[j]]["stds"] = Math.sqrt(m2 / (n - 1));
+                                }
+                                if (results[i].aggregate[indexes[j]]["sum"] !== undefined) {
+                                    aggregate[indexes[j]]["mean"] = (aggregate[indexes[j]]["sum"] + results[i].aggregate[indexes[j]]["sum"]) / (aggregate[indexes[j]]["count"] + results[i].aggregate[indexes[j]]["count"]);
+                                    aggregate[indexes[j]]["sum"] += results[i].aggregate[indexes[j]]["sum"];
+                                }
+                                aggregate[indexes[j]]["count"] += results[i].aggregate[indexes[j]]["count"];
+                            } else {
+                                aggregate[indexes[j]] = {
+                                    field: results[i].aggregate[indexes[j]]["field"],
+                                    min: results[i].aggregate[indexes[j]]["min"],
+                                    max: results[i].aggregate[indexes[j]]["max"],
+                                    count: results[i].aggregate[indexes[j]]["count"],
+                                    sum:  (results[i].aggregate[indexes[j]]["sum"] !== undefined) ? results[i].aggregate[indexes[j]]["sum"] : undefined,
+                                    mean:  (results[i].aggregate[indexes[j]]["mean"] !== undefined) ? results[i].aggregate[indexes[j]]["mean"] : undefined,
+                                    varp: (results[i].aggregate[indexes[j]]["m2"] !== undefined) ? results[i].aggregate[indexes[j]]["m2"] / results[i].aggregate[indexes[j]]["count"] : undefined,
+                                    vars: (results[i].aggregate[indexes[j]]["m2"] !== undefined) ? results[i].aggregate[indexes[j]]["m2"] / (results[i].aggregate[indexes[j]]["count"] - 1) : undefined,
+                                    stdp: (results[i].aggregate[indexes[j]]["m2"] !== undefined) ? Math.sqrt(results[i].aggregate[indexes[j]]["m2"] / results[i].aggregate[indexes[j]]["count"]) : undefined,
+                                    stds: (results[i].aggregate[indexes[j]]["m2"] !== undefined) ? Math.sqrt(results[i].aggregate[indexes[j]]["m2"] / (results[i].aggregate[indexes[j]]["count"] - 1)) : undefined,
+                                    m2: (results[i].aggregate[indexes[j]]["m2"] !== undefined) ? results[i].aggregate[indexes[j]]["m2"] : undefined
+                                };
+                            }
+                        }
+                        if (results[i].start < start) start = results[i].start;
+                        if (results[i].end > end) end = results[i].end;
+                    }
+                    resolve({data: Object.keys(aggregate).map(index => {return {index: index, field: aggregate[index].field, min: aggregate[index].min, max: aggregate[index].max, count: aggregate[index].count, sum: aggregate[index].sum, mean: aggregate[index].mean, varp: (aggregate[index].count > 1) ? aggregate[index].varp : undefined, vars: (aggregate[index].count > 1) ? aggregate[index].vars : undefined, stdp: (aggregate[index].count > 1) ? aggregate[index].stdp : undefined, stds: (aggregate[index].count > 1) ? aggregate[index].stds : undefined}}), start: start, end: end, details: results})
+                })
+                .catch(error => reject(error));
+        });
+
+    }
+
     index(field) {
         return new Promise ((resolve, reject) => {
             var promises = [];
